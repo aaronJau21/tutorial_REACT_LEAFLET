4413a45d925328ed1fa7b277ae1016cbe85b5729
aaronJau21 <aaronjau21@gmail.com>
2023-12-07 11:56:28 -0500

    Vectores y Popups

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
-// import MostrarMap from "./components/MostrarMap";
+import MostrarMap from "./components/MostrarMap";
 import LayersControl from "./components/LayersControl2";
+import Vectores from "./components/Vectores";
+import Popups from "./components/Popups";
 
 
 
 const router = createBrowserRouter([
-  // {
-  //   path: '/',
-  //   element: <MostrarMap />
-  // },
+  {
+    path: '/',
+    element: <MostrarMap />
+  },
   {
     path: '/LayersControl',
     element: <LayersControl />
+  },
+  {
+    path: '/vectores',
+    element: <Vectores />
+  },
+  {
+    path: '/popups',
+    element: <Popups />
   }
 ])
 
